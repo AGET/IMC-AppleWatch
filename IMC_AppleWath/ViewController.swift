2bc7308b4fe3b7d2f244d8799584bc738860a679
AGET <aldoget1992@gmail.com>
2017-04-25 01:02:57 -0500

    Initial Commit

---
+++ IMC_AppleWath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMC_AppleWath
+//
+//  Created by Anzen on 25/04/17.
+//  Copyright © 2017 aget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
